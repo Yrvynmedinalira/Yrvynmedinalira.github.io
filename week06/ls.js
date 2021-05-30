0aead60e88d061f4d4aa25303004fb199fac16d0
Yrvynmedinalira <med19007@byui.edu>
2021-05-30 00:10:48 -0700

    up laod

---
+++ week06/ls.js
@@ -0,0 +1,31 @@
+// This is to clear local storage
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", function () {
+  localStorage.clear();
+  location.reload();
+})
+
+// This is to set local storage
+function toLocalStorage(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// This is to remove form local storage
+function removeLocalStorage(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
