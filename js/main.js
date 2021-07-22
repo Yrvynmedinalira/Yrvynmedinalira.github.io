eb742facfcaec33d8935cdc84b9e7c3170e2dd91
Yrvynmedinalira <med19007@byui.edu>
2021-07-21 22:35:51 -0700

    uplaod

--- js/main.js
+++ js/main.js
@@ -1,46 +1,46 @@
 const links = [
   {
-    label: "Week01 notes",
+    label: "Week01",
     url: "week01/index.html"
   },
   {
-    label: "Week02 notes",
+    label: "Week02",
     url: "week02/index.html"
   },
   {
-    label: "Week03 notes",
+    label: "Week03",
     url: "week03/index.html"
   },
   {
-    label: "Week04 notes",
+    label: "Week04",
     url: "week04/index.html"
   },
   {
-    label: "Week05 notes",
+    label: "Week05",
     url: "week05/index.html"
   },
   {
-    label: "Week06 notes",
+    label: "Week06",
     url: "week06/index.html"
   },
   {
-    label: "Week07 notes",
+    label: "Week07",
     url: "week07/index.html"
   },
   {
-    label: "Week08 notes",
+    label: "Week08",
     url: "week08/index.html"
   },
   {
-    label: "Week09 notes",
+    label: "Week09",
     url: "week09/index.html"
   },
   {
-    label: "Week10 notes",
+    label: "Week10",
     url: "week10/index.html"
   },
   {
-    label: "Week11 notes",
+    label: "Week11",
     url: "week11/index.html"
   }, 
   {
